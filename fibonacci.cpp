a7d96e6e5bb440ffe5b1cad6fafb9dcc149c6575
stoody255 <stefan.minhui@gmail.com>
2026-01-16 22:35:58 +0100

    fibonacci result

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 
-
-
 int main() {
-// std::vector
- std::cout << "Hello World\n";
-
+  std::vector<int> x = {0, 1};
+  std::cout << "0, 1";
+  for (int i = 0; i <= 18; i++) {
+    x.push_back(x[i] + x[i+1]);
+    std::cout << ", " <<  x[i+2];
+  }
+  std::cout << "\n";
 }
